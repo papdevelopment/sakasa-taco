657f44031f6e19f5ae9fa7b6c7c7e7bae10f9c88
SAKASA A.Ş <127118520+papdevelopment@users.noreply.github.com>
2023-06-17 19:49:10 +0300

    Fivem Taco Hobby Occupation

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+
+
+fx_version 'adamant'
+
+game 'gta5'
+
+this_is_a_map 'yes'
+
+client_scripts {
+    'client/main.lua',
+    "satcl.lua",
+    "config.lua"
+}
+
+server_scripts {
+    'server/main.lua',
+    "satsv.lua",
+    "config.lua"
+}
